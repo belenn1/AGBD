ce3f3781cc20f6a23daff10fee024e44b5d8a718
Belenn <belennnc25026@gmail.com>
2024-05-02 12:09:15 -0300

    Hice del ejercicio 1 al 4

--- Simulacro.sql
+++ Simulacro.sql
@@ -12,8 +12,14 @@ WHERE rating LIKE "%PG%"
 ORDER BY rental_rate DESC, title ASC
 
 4
+SELECT city, country FROM city t
+JOIN country c ON t.country_id = c.country_id 
+ORDER BY c.country_id
 
 5
+SELECT country, city FROM city t
+JOIN country c ON t.country_id = c.country_id
+GROUP BY city  /*falta completar*/
 
 6
 
