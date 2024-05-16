c5a1e0884021e4c5ae5f93277f0df31d2718d643
belenn <belennnc25026@gmail.com>
2024-05-16 11:46:56 -0300

    Ejercicio 2-evaluacion

--- evaluacion.sql
+++ evaluacion.sql
@@ -3,6 +3,10 @@ SELECT DISTINCT type FROM crime_scene_report
 ORDER BY type ASC
 
 2
+SELECT name,age,gender,car_make,car_model,plate_number FROM person p
+JOIN drivers_license d on p.license_id = d.id
+WHERE gender = 'female' AND age < 40
+ORDER by name asc
 
 3
 
