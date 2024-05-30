8eb0ed990858e5b3fd077ba1b7eae8a3c6a0084b
Belenn <belennnc25026@gmail.com>
2024-05-30 09:56:11 -0300

    Ejercicio de INSERT

---
+++ EjercicioModificaciones.sql
@@ -0,0 +1,8 @@
+INSERT INTO artists(name)
+VALUES("Mitski")
+
+INSERT INTO albums(Title,ArtistId)
+VALUES("the land is inhospitable and so are we",277)
+
+INSERT INTO tracks(name,MediaTypeId,Milliseconds,UnitPrice)
+VALUES("Star",5,180000,0.99),("My love mine all mine ",5,150000,0.99),("i don't like my mind ",5,135600,0.99)
