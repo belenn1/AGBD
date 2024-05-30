731b5a378b7c7dbeb10992df9d6603ad5bda4d24
Belenn <belennnc25026@gmail.com>
2024-05-30 11:38:26 -0300

    Ejercicios de INSERT,UPDATE,DELETE

--- EjercicioModificaciones.sql
+++ EjercicioModificaciones.sql
@@ -6,3 +6,15 @@ VALUES("the land is inhospitable and so are we",277)
 
 INSERT INTO tracks(name,MediaTypeId,Milliseconds,UnitPrice)
 VALUES("Star",5,180000,0.99),("My love mine all mine ",5,150000,0.99),("i don't like my mind ",5,135600,0.99)
+
+UPDATE tracks SET AlbumId = 348,Composer = "Mitski"
+WHERE  GenreId is NULL;
+
+DELETE FROM tracks
+WHERE Composer = "Mitski"
+
+DELETE FROM albums
+WHERE Title = "the land is inhospitable and so are we"
+
+DELETE FROM artists
+WHERE name = "Mitski"
